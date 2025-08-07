82d16c2d699428fa37ef6eff9e5262d59143cd6e
02Kani <kanikaradag02@gmail.com>
2025-08-07 14:49:03 +0200

    some simple linked lists

---
+++ src/NodeMain.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class NodeMain {
+	public static void main(String[] args) {
+		Node head = new Node(0);
+		Node nodeA = new Node(1);
+		Node nodeB = new Node(2);
+		Node nodeC = new Node(3);
+		Node nodeD = new Node(4);
+		Node nodeE = new Node(5);
+		
+		head.next = nodeA;
+		nodeA.next = nodeB;
+		nodeB.next = nodeC;
+		nodeC.next = nodeD;
+		nodeD.next = nodeE;
+		
+		// findNodePos(head, NodeD)
+	}
+	
+	
+	// Finde die Position des Nodes 
+	static void findNodePos(Node head, Node node) {
+		
+		Node currentNode = head;
+		Node targetNode = node;
+		int counter = 1;
+		
+		while(currentNode.data != targetNode.data) {
+			counter++;
+			currentNode = currentNode.next;
+		}
+		
+		System.out.println("Position: " + counter);
+	}
+	
+	
+	// 
+}
